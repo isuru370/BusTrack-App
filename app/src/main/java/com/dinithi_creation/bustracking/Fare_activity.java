6b796a4bbf827130f585269a30da0b8016b95923
fernando <isurufernando000@yahoo.com>
2022-10-12 12:49:18 +0530

    new activity update

---
+++ app/src/main/java/com/dinithi_creation/bustracking/Fare_activity.java
@@ -0,0 +1,14 @@
+package com.dinithi_creation.bustracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fare_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fare);
+    }
+}
