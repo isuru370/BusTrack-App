ea27ab01311c35ea1cba283f61cc3c415cb029c2
fernando <isurufernando000@yahoo.com>
2022-10-13 11:28:19 +0530

    dally update one activity disaing

---
+++ app/src/main/java/com/dinithi_creation/bustracking/Busregister_activity.java
@@ -0,0 +1,14 @@
+package com.dinithi_creation.bustracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Busregister_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busregister);
+    }
+}
